08e0c6d00f9943fc6591f0af73578e74e570a8da
Gismar <thegismar@gmail.com>
2021-01-22 01:26:45 -0300

    meh

---
+++ README.md
@@ -0,0 +1,8 @@
+### Very simple eth vanity wallet generator
+
+
+`pip install -r requirements`
+
+`python vanity_accounts.py`
+
+Enter the first chars that you want your public address to have (incl. 0x)
--- vanity_accounts.py
+++ vanity_accounts.py
@@ -2,30 +2,26 @@ from web3  import Web3
 from tqdm import tqdm
 
 w3 = Web3()
-w3.eth.account.enable_unaudited_hdwallet_features()
-# fill your list with possible vanity words, do or do not include the 0x
-vanity = ['0xb00b135']
 
-# this is just timer
+vanity = str(input('starting chars incl 0x: '))
+
 t = tqdm()
-
 while True:
-    # update
     t.update()
 
-    # w3 creates account
-    account = w3.eth.account.create_with_mnemonic(num_words=12)
+    account = w3.eth.account.create()
 
-    # tell it where you want the vanity part to be
-    pub_key = str(account[0].address).lower()[0:9]
+    end = len(vanity)
+    pub_key = str(account.address).lower()[0:end]
 
     '''
     important to check this way and not the other, if you do a check for each item of the list performance is
     super fucked
     '''
-
-    if pub_key in vanity[0]:
-        print(f'found this account: {pub_key, account[1]}')
+    if pub_key in vanity:
+        print(f'found this account: {account.address, Web3.toHex(account.privateKey)}')
+        with open('keys', 'a') as f:
+            f.write(account.address + ', ' + Web3.toHex(account.privateKey) + '\n')
         break
     else:
         continue
