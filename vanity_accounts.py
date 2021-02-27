9478235b0e54edad76203c8ce3a5a6685a182edb
Gismar <thegismar@gmail.com>
2021-02-27 03:01:32 -0300

    changed to generate seed phrase..

--- vanity_accounts.py
+++ vanity_accounts.py
@@ -1,27 +1,30 @@
-from web3  import Web3
+from web3 import Web3
 from tqdm import tqdm
 
 w3 = Web3()
 
-vanity = str(input('starting chars incl 0x: '))
+vanity = str( input( 'starting chars incl 0x: ' ) )
+w3.eth.account.enable_unaudited_hdwallet_features()
 
 t = tqdm()
 while True:
     t.update()
 
-    account = w3.eth.account.create()
+    account = w3.eth.account.create_with_mnemonic()
 
-    end = len(vanity)
-    pub_key = str(account.address).lower()[0:end]
+    end = len( vanity )
+    pub_key = str( account[0].address ).lower()[0:end]
 
     '''
     important to check this way and not the other, if you do a check for each item of the list performance is
     super fucked
     '''
     if pub_key in vanity:
-        print(f'found this account: {account.address, Web3.toHex(account.privateKey)}')
-        with open('keys', 'a') as f:
-            f.write(account.address + ', ' + Web3.toHex(account.privateKey) + '\n')
+        result = f' found this account: {account[0].address, account[1]}'
+        print( result )
+
+        with open( 'keys', 'a' ) as f:
+            f.write( result )
         break
     else:
         continue
